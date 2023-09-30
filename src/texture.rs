29213b863a6f62a2cda44c1480bdd73f4e5559b1
saltyfishie-fedora <teugene888@gmail.com>
2023-09-30 23:59:24 +0800

    learn-wgpu: Uniform buffers and a 3d camera

--- src/texture.rs
+++ src/texture.rs
@@ -33,7 +33,7 @@ impl Texture {
         };
 
         let texture = device.create_texture(&wgpu::TextureDescriptor {
-            label: Some("diffuse_texture"),
+            label,
             size: texture_size,
             mip_level_count: 1,
             sample_count: 1,
