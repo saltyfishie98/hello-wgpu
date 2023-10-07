40102d0ab288f9e73e8055d36affadc4f089ed61
saltyfishie-fedora <teugene888@gmail.com>
2023-10-07 10:22:12 +0800

    update logging format

--- src/helpers.rs
+++ src/helpers.rs
@@ -2,10 +2,11 @@ pub fn setup_logging() {
     fern::Dispatch::new()
         .format(|out, message, record| {
             out.finish(format_args!(
-                "[{} {} {}]\n{}\n",
+                "[{} {}] {}::{}\n- {}\n",
                 chrono::Local::now().format("%Y-%m-%d %H:%M:%S"),
                 record.level(),
                 record.target(),
+                record.line().unwrap_or_default(),
                 message
             ))
         })
